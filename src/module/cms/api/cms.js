4c498da34d163cfb7f3a5450b7822900fdef2059
hcq <hcq0514@163.com>
2019-05-28 17:40:06 +0800

    CmsPage delete

--- src/module/cms/api/cms.js
+++ src/module/cms/api/cms.js
@@ -6,12 +6,16 @@ let apiUrl = sysConfig.xcApiUrlPre
 
 // 获取cmsPage列表
 export const searchCmsPageList = (page, size, params) => {
-  return http.requestQuickGet('http://api.mooc.com/api/cms/cmsPage/list/'+page+'/'+size)
+  return http.requestQuickGet('http://api.mooc.com/api/cms/cmsPage/list/' + page + '/' + size)
 }
 // 添加cmsPage页面
 export const addCmsPage = (params) => {
-  return http.requestPost('http://api.mooc.com/api/cms/cmsPage/add',params);
+  return http.requestPost('http://api.mooc.com/api/cms/cmsPage/add', params)
   // return http.requestPostForm('http://api.mooc.com/api/cms/cmsPage/add',params)
+}
+// 删除cmsPage
+export const deleteCmsPage = (pageId) => {
+  return http.requestDelete('http://api.mooc.com/api/cms/cmsPage/delete/' + pageId)
 }
 
 //获取cmsSite列表
@@ -19,7 +23,7 @@ export const searchAllCmsSite = () => {
   return http.requestQuickGet('http://api.mooc.com/api/cms/cmsSite/all')
 }
 //获取cmsTemplate列表
-export const searchAllCmsTemplate= () => {
+export const searchAllCmsTemplate = () => {
   return http.requestQuickGet('http://api.mooc.com/api/cms/cmsTemplate/all')
 }
 
--- src/module/cms/page/page_list.vue
+++ src/module/cms/page/page_list.vue
@@ -26,6 +26,20 @@
       <!--      </el-table-column>-->
       <el-table-column prop="pageCreateTime" label="创建时间">
       </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="handleDelete(scope.$index, scope.row)">编辑
+          </el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.row.pageId)">删除
+          </el-button>
+        </template>
+      </el-table-column>
+
     </el-table>
     <el-pagination
       style="float: right"
@@ -63,6 +77,22 @@
       handleCurrentChange (page) {
         this.params.page = page
         this.queryCmsPageList()
+      },
+      handleDelete (pageId) {
+        //确认提交
+        this.$confirm('确认删除?', '提示', {confirmButtonText: '确定', cancelButtonText: '取消', type: 'warning'})
+          .then(() => {
+            cmsApi.deleteCmsPage(pageId).then(res => {
+              if (!res.success) {
+                //删除失败
+                this.$message({type: 'error', message: '删除失败，' + res.message})
+                this.queryCmsPageList();
+              } else {
+                this.$message({type: 'success', message: '删除成功!'})
+                this.queryCmsPageList();
+              }
+            })
+          })
       }
     },
     //钩子函数，一进来默认查询页面
